4dc300c12d3aa5c8fd6377ab094dd9a319afa326
kkiriyama <kurata-nobuhiro901@g.ecc.u-tokyo.ac.jp>
2018-12-22 12:25:42 +0900

    fix bug

--- utils.py
+++ utils.py
@@ -3,10 +3,11 @@ import pandas as pd
 import os
 import glob
 from PIL import Image
+from tqdm import tqdm
 
 for i in range(1, 8):
   files = glob.glob('./train/location%s/images/*.jpg'%(i))
-  for f in files:
+  for f in tqdm(files):
     img = Image.open(f)
-    img_resize = img.resize((192, 192))
+    img_resize = img.resize((416, 416))
     img_resize.save(f)
