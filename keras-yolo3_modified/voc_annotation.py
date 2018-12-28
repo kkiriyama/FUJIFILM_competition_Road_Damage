2e1e96bf04c7705a47e793ef16123bd620390d88
kkiriyama <kurata-nobuhiro901@g.ecc.u-tokyo.ac.jp>
2018-12-29 08:56:57 +0900

    scaled annotation

--- keras-yolo3_modified/voc_annotation.py
+++ keras-yolo3_modified/voc_annotation.py
@@ -3,6 +3,7 @@ import os
 from os import getcwd
 
 sets=range(1, 8)
+scale = 0.05
 
 def convert_annotation(location_n, image_id, list_file):
     image_id = image_id.split('.')[0]
@@ -13,12 +14,20 @@ def convert_annotation(location_n, image_id, list_file):
     for obj in root.iter('object'):
         cls_id = obj.find('name').text
         xmlbox = obj.find('bndbox')
-        b = (int(int(xmlbox.find('xmin').text) * 0.693), int(int(xmlbox.find('ymin').text) * 0.693), int(int(xmlbox.find('xmax').text) * 0.693), int(int(xmlbox.find('ymax').text) * 0.693))
+        xmin = int(xmlbox.find('xmin').text) * 0.693
+        ymin = int(xmlbox.find('ymin').text) * 0.693
+        xmax = int(xmlbox.find('xmax').text) * 0.693
+        ymax = int(xmlbox.find('ymax').text) * 0.693
+        scaled_xmin = int((((xmin + xmax) / 2 - xmin) * 0.05) + xmin)
+        scaled_ymin = int((((ymin + ymax) / 2 - ymin) * 0.05) + ymin)
+        scaled_xmax = int(xmax - (((xmin + xmax) / 2 - xmin) * 0.05))
+        scaled_ymax = int(ymax - (((xmin + xmax) / 2 - xmin) * 0.05))
+        b = (scaled_xmin, scaled_ymin, scaled_xmax, scaled_ymax)
         list_file.write(" " + ",".join([str(a) for a in b]) + ',' + str(cls_id))
 
 wd = getcwd()
 
-list_file = open('train.txt', 'w')
+list_file = open('train_scaled.txt', 'w')
 for location_n in sets:
     image_ids = os.listdir('./train/location%d/images'%(location_n))
     for image_id in image_ids:
