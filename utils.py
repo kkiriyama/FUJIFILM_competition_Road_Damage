ba5ac630ed80802100fae5f081c17430717816ca
kkiriyama <kurata-nobuhiro901@g.ecc.u-tokyo.ac.jp>
2019-01-04 04:19:01 +0900

    fix bug

--- utils.py
+++ utils.py
@@ -6,9 +6,10 @@ from PIL import Image
 from tqdm import tqdm
 
 for i in range(1, 8):
-  files = os.listdir('../content/drive/My Drive/fujifilm/detection/keras-yolo3_modified/train/location%d/images'%(i))
+  PATH = '../content/drive/My Drive/fujifilm/detection/keras-0ylo3_modified/train/location%d/images'%(i)
+  files = os.listdir(PATH)
   print(len(files))
   for f in tqdm(files):
-    img = Image.open(f)
+    img = Image.open(os.path.join(PATH + f))
     img_resize = img.resize((416, 416))
     img_resize.save(f)
